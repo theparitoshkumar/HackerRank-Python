97e351f1c7594ec501c37f69dbc756dd0ed93c35
Paritosh Kumar <60515673+theparitoshkumar@users.noreply.github.com>
2021-12-01 12:26:53 +0000

    string concatenation

---
+++ 3. Strings/What's Your Name/name.py
@@ -0,0 +1,26 @@
+"""
+Link: https://www.hackerrank.com/challenges/whats-your-name/problem?isFullScreen=true
+
+Problem: What's Your Name?
+"""
+
+
+#
+# Complete the 'print_full_name' function below.
+#
+# The function is expected to return a STRING.
+# The function accepts following parameters:
+#  1. STRING first
+#  2. STRING last
+#
+
+def print_full_name(first, last):
+    # Write your code here
+    statement = "Hello " + first +" "+ last + "! You just delved into python."
+    print(statement)
+
+    
+if __name__ == '__main__':
+    first_name = input()
+    last_name = input()
+    print_full_name(first_name, last_name)
